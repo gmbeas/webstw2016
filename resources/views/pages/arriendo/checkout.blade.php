974741e8291c460fd2859a0b213513390e15d382
Gonzalo Martinez <gmbeas@gmail.com>
2016-09-26 14:58:41 -0300

    Actualizacion

--- resources/views/pages/arriendo/checkout.blade.php
+++ resources/views/pages/arriendo/checkout.blade.php
@@ -66,6 +66,7 @@
                         </div>
                     </div>
                     <!-- FIN REGISTRO - LOGIN -->
+                    {{ Form::open(array('class' => 'validar-formulario', 'id' => 'CompraConfirmarArriendoForm')) }}
                     <!-- DESPACHO -->
                     <div class="panel panel-default">
                         <div class="panel-heading active">
@@ -85,13 +86,13 @@
                                             <div class="col-md-4">
                                                 <div class="form-group">
                                                     <label>Nombre del Evento</label>
-                                                    {{ Form::input('eventonombre', 'text', null, ['class' => 'form-control', 'id' => 'eventonombre']) }}
+                                                    {{ Form::input('data[Evento][nombre]', 'text', null, ['class' => 'form-control', 'id' => 'eventonombre']) }}
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
                                                 <div class="form-group">
                                                     <label>Tipo de evento</label>
-                                                    {{ Form::select('eventotipo', array(
+                                                    {{ Form::select('data[Evento][tipo]', array(
                                                         '0' => '- seleccione tipo de evento',
                                                         'ANI' => 'ANIVERSARIO',
                                                         'BAN' => 'FIESTA PARTICULAR',
@@ -108,13 +109,13 @@
                                             <div class="col-md-2">
                                                 <div class="form-group">
                                                     <label>Fecha del Evento</label>
-                                                    {{ Form::input('text', 'eventofecha', null, ['class' => 'form-control datepicker', 'id' => 'eventofecha']) }}
+                                                    {{ Form::input('text', 'data[Evento][fecha]', null, ['class' => 'form-control datepicker', 'id' => 'eventofecha']) }}
                                                 </div>
                                             </div>
                                             <div class="col-md-2">
                                                 <div class="form-group">
                                                     <label>Días de arriendo</label>
-                                                    {{ Form::select('eventodias', $dias, null, ['class' => 'form-control', 'id' => 'eventodias']) }}
+                                                    {{ Form::select('data[Evento][dias]', $dias, null, ['class' => 'form-control', 'id' => 'eventodias']) }}
                                                 </div>
                                             </div>
                                         </div>
@@ -136,14 +137,11 @@
                                     <div class="col-md-12" id="despacho-domicilio" style="display: none;">
                                         <div class="row">
                                             <div class="col-md-12">
-                                                <a data-toggle="modal" href='#add-direc' class=" btn btn-xs btn-mega pull-right">Agregar dirección</a>
+                                                <a data-toggle="modal" href='#add-direc' class=" btn btn-xs btn-mega pull-right" style="margin-top:-10px">Agregar dirección</a>
                                             </div>
                                         </div>
-                                        <div class="form-group">
-                                            <label>Seleccione:</label>
-
-
-                                        </div>
+                                        <h6>Selecciona:</h6>
+                                        {{ Form::select('CompraDespachoId', $direcciones, null, ['class' => 'form-control', 'id' => 'CompraDespachoId']) }}
                                         <br>
                                         <table class="table table-condensed">
                                             <tr>
@@ -170,13 +168,13 @@
                                             <div class="col-md-4">
                                                 <div class="form-group">
                                                     <label>Fecha de entrega</label>
-                                                    {!! Form::input('text', 'fechaentrega', false, ['readonly', 'class' => 'form-control', 'id' => 'fechaentrega']) !!}
+                                                    {!! Form::input('text', 'data[Entrega][fecha]', false, ['readonly', 'class' => 'form-control', 'id' => 'fechaentrega']) !!}
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
                                                 <div class="form-group">
                                                     <label>Horario de entrega</label>
-                                                    {{ Form::select('horarioentrega', array(
+                                                    {{ Form::select('data[Entrega][horario]', array(
                                                                     '0' => '- seleccione horario entrega',
                                                                     'DD' => 'DD (Durante el día): 9:00 a 19:00 hrs.',
                                                                     'DM' => 'DM (Durante la mañana): 9:00 a 14:00 hrs.',
@@ -202,14 +200,14 @@
                                             <div class="col-md-4">
                                                 <div class="form-group">
                                                     <label>Fecha de retorno</label>
-                                                    {!! Form::input('text', 'fecharetorno', false, ['readonly', 'class' => 'form-control', 'id' => 'fecharetorno']) !!}
+                                                    {!! Form::input('text', 'data[Retorno][fecha]', false, ['readonly', 'class' => 'form-control', 'id' => 'fecharetorno']) !!}
 
                                                 </div>
                                             </div>
                                             <div class="col-md-4">
                                                 <div class="form-group">
                                                     <label>Horario de retorno</label>
-                                                    {{ Form::select('horarioretorno', array(
+                                                    {{ Form::select('data[Retorno][horario]', array(
                                                                 '0' => '- seleccione horario retorno',
                                                                 'DD' => 'DD (Durante el día): 9:00 a 19:00 hrs.',
                                                                 'DM' => 'DM (Durante la mañana): 9:00 a 14:00 hrs.',
@@ -237,23 +235,60 @@
                         </div>
                     </div>
                     <!-- FIN DESPACHO -->
-                    <!-- PAGO -->
+                    <!-- INFORMACION -->
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title">
                                 <a class="accordion-toggle" href="#collapseFive">
-                                    <span>3</span>Forma de Pago
+                                    <span>3</span>Información
                                 </a>
                             </h4>
                         </div>
                         <div id="collapseFive" class="panel-collapse collapse">
                             <div class="panel-body">
-                                <input type="radio" name="mediopago" id="mediopago" value="1" checked="checked" style="margin-right: 10px;"> Web Play Plus
-                                <a href="#collapseSix" class=" btn btn-mega pull-right">Continuar</a>
+                                <div class="row">
+                                    <h3>Información de contacto</h3>
+                                    <div class="col-md-12 m-bot">
+                                        <div class="row">
+                                            <div class="col-md-5">
+                                                <div class="form-group">
+                                                    <label>Nombre de contacto</label>
+                                                    {{ Form::input('contactonombre', 'text', null, ['class' => 'form-control', 'id' => 'contactonombre']) }}
+                                                </div>
+                                            </div>
+                                            <div class="col-md-5 col-md-offset-2">
+                                                <div class="form-group">
+                                                    <label>Teléfono de contacto</label>
+                                                    {{ Form::input('contactofono', 'text', null, ['class' => 'form-control', 'id' => 'contactofono', 'placeholder' => 'Ej: 0977777777']) }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 m-bot">
+                                        <div class="row">
+                                            <div class="col-md-5">
+                                                <div class="form-group">
+                                                    <label>Email de contacto</label>
+                                                    {{ Form::input('email', 'contactomail', null, ['class' => 'form-control', 'id' => 'contactomail', 'placeholder' => 'Ej: email@email.com']) }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 m-bot">
+                                        <div class="form-group">
+                                            <label>Comentario</label>
+                                            {{ Form::textarea('contactocomentario', null, ['class' => 'form-control', 'id' => 'contactocomentario']) }}
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <a href="#collapseSix" class=" btn btn-mega pull-right">Continuar</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <!-- FIN PAGO -->
+                    <!-- FIN INFORMACION -->
                     <!-- RESUMEN -->
                     <div class="panel panel-default">
                         <div class="panel-heading">
@@ -323,11 +358,14 @@
 
                                                 <div class="col-md-12" style="margin-bottom: 20px;">
                                                     <div class="row">
-                                                        {{Form::checkbox('politicas', 'value', false, ['id' => 'politicas'])}}
-                                                        <a href="" target="_blank" style="margin-left: 7px;">acepto los términos y condiciones</a>
+                                                        <a href="#" id="continuar" class="continuar btn btn-mega">Enviar solicitud</a>
                                                     </div>
                                                 </div>
-                                                <a href="#" id="continuar" class="continuar btn btn-mega">Pagar</a>
+                                                <div class="col-md-12">
+                                                    <div class="row">
+                                                        <p>Nos contactaremos con usted a la brevedad para formalizar su pedido.</p>
+                                                    </div>
+                                                </div>
                                             </div>
                                             <div class="clearfix"></div>
 
@@ -338,6 +376,7 @@
                         </div>
                     </div>
                     <!-- FIN RESUMEN -->
+                    {{ Form::close() }}
                 </div>
                 <!-- end check out -->
                 <div class="divider divider-sm visible-sm  visible-xs"></div>
@@ -402,6 +441,21 @@
 @section('javascript')
     <script>
         var token = $('meta[name="csrf-token"]').attr('content');
+
+
+        function desplegar_despacho() {
+            $('#collapseFive').collapse('hide');
+            $('a[href="#collapseFive"]').parents('.panel-heading').removeClass('active');
+            $('#collapseSix').collapse('hide');
+            $('a[href="#collapseSix"]').parents('.panel-heading').removeClass('active');
+            if ($('#collapseThree #retirotienda').is(':checked')) {
+                $('#collapseThree #despacho-domicilio').slideUp(900);
+            } else {
+                $('#collapseThree #despacho-domicilio').slideDown(900);
+            }
+        }
+
+
         $(document).ready(function() {
             $('#eventofecha').datepicker({
                 autoclose : true,
@@ -411,6 +465,12 @@
             });
 
 
+
+            //desplegar_despacho();
+
+            $('#collapseThree #retirotienda').change(function() {
+                desplegar_despacho();
+            });
 
 
             $('#eventofecha, #eventodias').change(function() {
@@ -473,6 +533,89 @@
                 }).datepicker('update', fechaRetiro);
             });
 
+
+
+
+            $(document).on('click','a[href="#collapseFive"]',function(e) {
+                e.preventDefault();
+                var despacho = $('#CompraConfirmarArriendoForm #CompraDespachoId');
+                var formulario =  $('#CompraConfirmarArriendoForm');
+                if (! $(despacho).length) {
+                    return false;
+                }
+
+                $('#CompraConfirmarArriendoForm #collapseThree .form-control[name^="data[Evento]"]').each(function(index, elemento) {
+                    $(elemento).parents('.form-group').removeClass('has-error');
+                    if (! $(elemento).val()) {
+                        $(elemento).parents('.form-group').addClass('has-error');
+                    }
+                });
+
+                $('#CompraConfirmarArriendoForm #collapseThree .form-control[name^="data[Entrega]"]').each(function(index, elemento) {
+                    $(elemento).parents('.form-group').removeClass('has-error');
+                    if (! $(elemento).val()) {
+                        $(elemento).parents('.form-group').addClass('has-error');
+                    }
+                });
+
+                $('#CompraConfirmarArriendoForm #collapseThree .form-control[name^="data[Retorno]"]').each(function(index, elemento) {
+                    $(elemento).parents('.form-group').removeClass('has-error');
+                    if (! $(elemento).val()) {
+                        $(elemento).parents('.form-group').addClass('has-error');
+                    }
+                });
+
+                if ($(formulario).find('.has-error').length) {
+                    $.alerta('Para enviar la cotización debe llenar todos los campos destacados.');
+                    return false;
+                }
+
+                if (! $('#collapseThree #CompraRetiroTienda').is(':checked')) {
+                    if (! $(despacho).val()) {
+                        $.alerta('Debe seleccionar una dirección.');
+                        return false;
+                    }
+                }
+
+                $('a[href="#collapseFive"]').parents('.panel-heading').addClass('active');
+                $('#collapseFive').collapse('show');
+
+                $('html, body').animate({
+                    scrollTop: $("#collapseFive").parent().offset().top
+                }, 600);
+            });
+
+            $(document).on('click','a[href="#collapseSix"]',function(e) {
+                e.preventDefault();
+                if (! $('#collapseFive').hasClass('in')) {
+                    return false;
+                }
+                // validar formulario evento
+                if (! $('#collapseFive .form-control').length) {
+                    return false;
+                }
+                $('#collapseFive .has-error').removeClass('has-error');
+                $('#collapseFive .form-control').each(function(index, elemento) {
+                    if (! $(elemento).val()) {
+                        $(elemento).parents('.form-group').addClass('has-error');
+                    }
+                });
+                if ($('#collapseFive .has-error').length) {
+                    $.alerta('Para continuar debe ingresar los campos destacados.');
+                    return false;
+                }
+                // validar email
+                if (! $('#collapseFive #contactomail').isEmail()) {
+                    $('#collapseFive #contactomail').parents('.form-group').addClass('has-error');
+                    $.alerta('Para continuar debe ingresar un email válido.');
+                    return false;
+                }
+                $('a[href="#collapseSix"]').parents('.panel-heading').addClass('active');
+                $('#collapseSix').collapse('show');
+                $('html, body').animate({
+                    scrollTop: $("#collapseSix").parent().offset().top
+                }, 600);
+            });
         });
 
     </script>
